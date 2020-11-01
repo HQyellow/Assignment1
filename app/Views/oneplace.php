87190b0d4dc9cf76169440fff6e7707ed63f7350
HQyellow <1401362966@qq.com>
2020-11-01 23:57:43 +0800

    lab5

--- app/Views/oneplace.php
+++ app/Views/oneplace.php
@@ -1,14 +1,19 @@
 <!doctype html>
 <html>
 	<head>
-		<title>DGPT4711 Lab 04</title>
+		<title>DGPT4711 Assignment1</title>
 	</head>
 	<body>
-		<h1>One Travel Destinations</h1>
+		<h1>Forrest Gump Character Introduction</h1>
 		<p><strong>ID:</strong> {id}</p>
 		<p><strong>Name:</strong> {name}</p>
-		<p><strong>Description:</strong> {description}</p>
-		<p><strong>Tourism site:</strong> {link}</p>
+                <p><strong>Sex:</strong> {sex}</p>
+		<p><strong>Country:</strong> {Country}</p>
+		<p><strong>Hobby:</strong> {Hobby}</p>
+                <p><strong>Favorite food:</strong> {chocolate}</p>
+		<p><strong>Rank:</strong> {Rank}</p>
+		<p><strong>Life motto:</strong> {Life motto}</p>
+                <p><strong>Character photos:</strong> </p>
 		<p><img src="/image/{image}"/></p>
 		<p><a href="/home">Home</a></p>
 	</body>
